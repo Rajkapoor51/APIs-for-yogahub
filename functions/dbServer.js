26312dfc67dd9892b845abc728255ea4238b7635
Raj kapoor <89578075+Rajkapoor51@users.noreply.github.com>
2025-02-17 15:02:39 +0530

    Update dbServer.js

--- functions/dbServer.js
+++ functions/dbServer.js
@@ -141,7 +141,7 @@ router.post("/upload-image", uploadImage.single("image"), (req, res) => {
 
   res.json({
     message: "Image uploaded successfully!",
-    imageUrl: `http://localhost:3000/uploads/images/${req.file.filename}`
+    imageUrl: `https://yogahubapis.netlify.app/uploads/images/${req.file.filename}`
   });
 });
 
@@ -153,7 +153,7 @@ router.post("/upload-video", uploadVideo.single("video"), (req, res) => {
 
   res.json({
     message: "Video uploaded successfully!",
-    videoUrl: `http://localhost:3000/uploads/videos/${req.file.filename}`
+    videoUrl: `https://yogahubapis.netlify.app/uploads/videos/${req.file.filename}`
   });
 });
 
@@ -166,7 +166,7 @@ router.get("/images", (req, res) => {
       return res.status(500).json({ message: "Error retrieving images" });
     }
 
-    const images = files.map(file => `http://localhost:3000/uploads/images/${file}`);
+    const images = files.map(file => `https://yogahubapis.netlify.app/uploads/images/${file}`);
     res.json({ images });
   });
 });
@@ -180,7 +180,7 @@ router.get("/videos", (req, res) => {
       return res.status(500).json({ message: "Error retrieving videos" });
     }
 
-    const videos = files.map(file => `http://localhost:3000/uploads/videos/${file}`);
+    const videos = files.map(file => `https://yogahubapis.netlify.app/uploads/videos/${file}`);
     res.json({ videos });
   });
 });
@@ -226,7 +226,7 @@ router.delete("/delete-video", (req, res) => {
 
 router.post("/add-offer", upload.single("image"), (req, res) => {
   const { title, description, discount } = req.body;
-  const imageUrl = req.file ? `http://localhost:3000/uploads/offers/${req.file.filename}` : ""; // Empty if no image
+  const imageUrl = req.file ? `https://yogahubapis.netlify.app/uploads/offers/${req.file.filename}` : ""; // Empty if no image
 
   if (!title || !description || !discount) {
     return res.status(400).json({ success: false, message: "Title, description, and discount are required." });
